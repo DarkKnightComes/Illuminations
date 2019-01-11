a90ddcc0b7ba1940b7f9e582c4f551895a5fe266
riverleviathan <riverleviathan@gmail.com>
2019-01-11 18:19:39 +0100

    Make the player able to spawn wisps without throwing them by sneaking

--- src/main/java/ladysnake/illuminations/common/items/WillOWispItem.java
+++ src/main/java/ladysnake/illuminations/common/items/WillOWispItem.java
@@ -6,7 +6,6 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.stat.Stats;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
@@ -18,22 +17,25 @@ public class WillOWispItem extends Item {
         super(settings);
     }
 
-    public TypedActionResult<ItemStack> use(World world_1, PlayerEntity playerEntity_1, Hand hand_1) {
-        ItemStack itemStack_1 = playerEntity_1.getStackInHand(hand_1);
-        if (!playerEntity_1.abilities.creativeMode) {
-            itemStack_1.subtractAmount(1);
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity player, Hand hand) {
+        ItemStack stackInHand = player.getStackInHand(hand);
+        if (!player.abilities.creativeMode) {
+            stackInHand.subtractAmount(1);
         }
 
-        world_1.playSound((PlayerEntity)null, playerEntity_1.x, playerEntity_1.y, playerEntity_1.z, SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
-        if (!world_1.isClient) {
-            WillOWispEntity wispThrown = new WillOWispEntity(world_1, playerEntity_1);
-            wispThrown.beingThrown = true;
-            wispThrown.calculateVelocity(playerEntity_1, playerEntity_1.pitch, playerEntity_1.yaw, 0.0F, 1.5F, 1.0F);
-            world_1.spawnEntity(wispThrown);
+        if (!player.isSneaking()) world.playSound((PlayerEntity)null, player.x, player.y, player.z, SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (random.nextFloat() * 0.4F + 0.8F));
+        if (!world.isClient) {
+            WillOWispEntity wispSpawned = new WillOWispEntity(world, player);
+            if (!player.isSneaking()) {
+                wispSpawned.beingThrown = true;
+                wispSpawned.calculateVelocity(player, player.pitch, player.yaw, 0.0F, 1.5F, 1.0F);
+            }
+            world.spawnEntity(wispSpawned);
         }
 
-        playerEntity_1.incrementStat(Stats.USED.method_14956(this));
-        return new TypedActionResult(ActionResult.SUCCESS, itemStack_1);
+        return new TypedActionResult(ActionResult.SUCCESS, stackInHand);
     }
+
+
 
 }
